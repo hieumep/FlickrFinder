88cf9b0b9065e4f00804e71fda22e6215ced00a1
RonaldVo <hieuvo@gmail.com>
2016-01-06 14:35:13 -0800

    add show/hide keyboard

--- FIickFinder/FilckFinderViewController.swift
+++ FIickFinder/FilckFinderViewController.swift
@@ -8,18 +8,88 @@
 
 import UIKit
 
-class FlickFinderViewController: UIViewController {
+class FlickFinderViewController: UIViewController,UITextFieldDelegate {
 
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var textImageView: UILabel!
+    @IBOutlet weak var textSearch: UITextField!
+    @IBOutlet weak var latTextSearch: UITextField!
+    @IBOutlet weak var LonTextSearch: UITextField!
+    @IBOutlet weak var photoImageTitle: UILabel!
+    
+    var tapRecognizer: UITapGestureRecognizer? = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        textSearch.delegate = self
+        latTextSearch.delegate = self
+        LonTextSearch.delegate = self
+        tapRecognizer = UITapGestureRecognizer(target: self, action: "handleSingleTap:")
+        tapRecognizer!.numberOfTapsRequired = 1
+    }
+    
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        addKeyboardDismissRecognizer()
+        subscribeToKeyboardNotifications()
+    }
+    
+    override func viewDidDisappear(animated: Bool) {
+        super.viewDidDisappear(animated)
+        removeKeyboardDismissRecognizer()
+        unsubscribeToKeyboadNotifications()
+    }
+    
+    func addKeyboardDismissRecognizer() {
+        self.view.addGestureRecognizer(tapRecognizer!)
+    }
+    
+    func removeKeyboardDismissRecognizer() {
+        self.view.removeGestureRecognizer(tapRecognizer!)
+    }
+    
+    func handleSingleTap(recognizer: UITapGestureRecognizer) {
+        self.view.endEditing(true)
+    }
+    
+    func subscribeToKeyboardNotifications(){
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillShow:", name: UIKeyboardDidShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillHide:", name: UIKeyboardDidHideNotification, object: nil)
+    }
+    
+    func unsubscribeToKeyboadNotifications(){
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardDidShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardDidHideNotification, object: nil)
+    }
+    
+    func keyboardWillShow(notification : NSNotification){
+        if (textSearch.editing)||(latTextSearch.editing)||(LonTextSearch.editing) {
+            view.frame.origin.y = getKeyboardHeight(notification) * -1
+        }
+    }
+    
+    func keyboardWillHide(notification: NSNotification){
+        view.frame.origin.y = 0
     }
 
+    //get height of Keyboard
+    func getKeyboardHeight(notification : NSNotification) -> CGFloat{
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIKeyboardFrameBeginUserInfoKey] as! NSValue
+        return keyboardSize.CGRectValue().size.height
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 
 }
 
